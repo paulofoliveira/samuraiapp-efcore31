e9bb60db4f26476cc3e9bfe80a22972c3a1f2e4e
Paulo Fernando <paulo.fdo@hotmail.com>
2020-02-25 14:34:57 -0300

    Relacionamentos

--- src/SamuraiApp.Data/SamuraiContext.cs
+++ src/SamuraiApp.Data/SamuraiContext.cs
@@ -10,10 +10,17 @@ namespace SamuraiApp.Data
         public DbSet<Clan> Clans { get; set; }
         public SamuraiContext()
         {
-
+            
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
             => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SamuraiAppData");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId }); // Composição para NxN entre Samurais e Battles
+
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
     }
 }
